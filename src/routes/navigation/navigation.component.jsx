a360a4500db3b344a4f26c8426c5a813729153a0
James Abilong <james.abilong@awsys-i.com>
2023-09-11 22:28:29 +0800

    Added initial cart feature

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,12 +3,15 @@ import { Outlet, Link } from "react-router-dom";
 import { ReactComponent as MainLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import { CartContext } from "../../contexts/cart.context";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 import "./navigation.styles.scss";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-
+  const { isCartOpen } = useContext(CartContext);
   return (
     <Fragment>
       <div className="navigation">
@@ -28,7 +31,9 @@ const Navigation = () => {
               Sign in
             </Link>
           )}
+          <CartIcon></CartIcon>
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,13 +1,16 @@
 import { useContext } from "react";
 import { ProductsContext } from "../../contexts/products.context";
+import ProductCard from "../../components/product-card/product-card.component";
+
+import "./shop.styles.scss";
 
 const Shop = () => {
   const { products } = useContext(ProductsContext);
   console.log(products);
   return (
-    <div>
-      {products.map(({ id, name }) => (
-        <div key={id}>{name}</div>
+    <div className="products-container">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product}></ProductCard>
       ))}
     </div>
   );
